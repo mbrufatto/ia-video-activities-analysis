195d798ef5897addfde78b3e3572eac48067b4bf
Michael Marques <michaelycus@gmail.com>
2025-02-06 09:19:58 -0300

    Comparing faces

--- tests/comparing_faces.py
+++ tests/comparing_faces.py
@@ -1,34 +1,86 @@
-from deepface import DeepFace
 import os
 
-def compare_faces(image1_path, image2_path):
+from deepface import DeepFace
+
+
+def compare_faces(image1, image2):
     try:
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        image1_path = os.path.join(script_dir, image1)
+        image2_path = os.path.join(script_dir, image2)
+
         # Realiza a verificação das imagens
-        result = DeepFace.verify(image1_path, image2_path)
-        
+        result = DeepFace.verify(
+            image1_path,
+            image2_path,
+            model_name="ArcFace",
+            detector_backend="retinaface",
+        )
+
         # Obtém se são a mesma pessoa e a distância de verificação
-        is_same_person = result['verified']
-        distance = result['distance']
-        
-        return {
-            'same_person': is_same_person,
-            'distance': distance,
-            'details': result
-        }
+        is_same_person = result["verified"]
+        distance = result["distance"]
+
+        print("\n" + image1 + " => " + image2)
+        print("São a mesma pessoa?", is_same_person)
+        print("Distância facial:", distance)
+
     except Exception as e:
-        return {'error': str(e)}
+        print("Erro ao comparar imagens:", str(e))
+
 
 if __name__ == "__main__":
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+    image1 = "person_1.jpg"
+    image2 = "person_2.jpg"
+    image3 = "person_3.jpg"
+    image4 = "person_4.jpg"
+    image5 = "person_5.jpg"
+    image6 = "person_6.jpg"
+    image7 = "person_7.jpg"
+    image8 = "person_8.jpg"
+    image9 = "person_9.jpg"
 
-    image1 = os.path.join(script_dir, 'michael1.jpg')
-    image2 = os.path.join(script_dir, 'michael3.jpg')
+    compare_faces(image1, image2)
+    compare_faces(image1, image3)
+    compare_faces(image1, image4)
+    compare_faces(image1, image5)
+    compare_faces(image1, image6)
+    compare_faces(image1, image7)
+    compare_faces(image1, image8)
+    compare_faces(image1, image9)
 
-    
-    comparison_result = compare_faces(image1, image2)
-    
-    if 'error' in comparison_result:
-        print("Erro ao comparar imagens:", comparison_result['error'])
-    else:
-        print("São a mesma pessoa?", comparison_result['same_person'])
-        print("Distância facial:", comparison_result['distance'])
+    compare_faces(image2, image3)
+    compare_faces(image2, image4)
+    compare_faces(image2, image5)
+    compare_faces(image2, image5)
+    compare_faces(image2, image6)
+    compare_faces(image2, image7)
+    compare_faces(image2, image8)
+
+    compare_faces(image3, image4)
+    compare_faces(image3, image5)
+    compare_faces(image3, image6)
+    compare_faces(image3, image7)
+    compare_faces(image3, image8)
+    compare_faces(image3, image9)
+
+    compare_faces(image4, image5)
+    compare_faces(image4, image6)
+    compare_faces(image4, image7)
+    compare_faces(image4, image8)
+    compare_faces(image4, image9)
+
+    compare_faces(image5, image6)
+    compare_faces(image5, image7)
+    compare_faces(image5, image8)
+    compare_faces(image5, image9)
+
+    compare_faces(image6, image7)
+    compare_faces(image6, image8)
+    compare_faces(image6, image9)
+
+    compare_faces(image7, image8)
+    compare_faces(image7, image9)
+
+    compare_faces(image8, image9)
